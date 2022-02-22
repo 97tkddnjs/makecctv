178eca9244d751755e2a94fd5e5cd0024599d6f5
Lee Sang Won <tkddnjs206@gmail.com>
2022-02-22 13:43:29 +0900

    bigfile

---
+++ pi/data.py
@@ -0,0 +1,42 @@
+# 학습시킨 데이터셋에 따른 신체부위 숫자와 이름
+BODY_PARTS_MPI = {0: "Head", 1: "Neck", 2: "RShoulder", 3: "RElbow", 4: "RWrist",
+                  5: "LShoulder", 6: "LElbow", 7: "LWrist", 8: "RHip", 9: "RKnee",
+                  10: "RAnkle", 11: "LHip", 12: "LKnee", 13: "LAnkle", 14: "Chest",
+                  15: "Background"}
+
+BODY_PARTS_COCO = {0: "Nose", 1: "Neck", 2: "RShoulder", 3: "RElbow", 4: "RWrist",
+                   5: "LShoulder", 6: "LElbow", 7: "LWrist", 8: "RHip", 9: "RKnee",
+                   10: "RAnkle", 11: "LHip", 12: "LKnee", 13: "LAnkle", 14: "REye",
+                   15: "LEye", 16: "REar", 17: "LEar", 18: "Background"}
+
+BODY_PARTS_BODY_25 = {0: "Nose", 1: "Neck", 2: "RShoulder", 3: "RElbow", 4: "RWrist",
+                      5: "LShoulder", 6: "LElbow", 7: "LWrist", 8: "MidHip", 9: "RHip",
+                      10: "RKnee", 11: "RAnkle", 12: "LHip", 13: "LKnee", 14: "LAnkle",
+                      15: "REye", 16: "LEye", 17: "REar", 18: "LEar", 19: "LBigToe",
+                      20: "LSmallToe", 21: "LHeel", 22: "RBigToe", 23: "RSmallToe", 24: "RHeel", 25: "Background"}
+
+# 연결되는 두 포인트
+POSE_PAIRS_MPI = [[0, 1], [1, 2], [1, 5], [1, 14], [2, 3], [3, 4], [5, 6],
+                  [6, 7], [8, 9], [9, 10], [11, 12], [12, 13], [14, 8], [14, 11]]
+
+POSE_PAIRS_COCO = [[0, 1], [0, 14], [0, 15], [1, 2], [1, 5], [1, 8], [1, 11], [2, 3], [3, 4],
+                   [5, 6], [6, 7], [8, 9], [9, 10], [12, 13], [11, 12], [14, 16], [15, 17]]
+
+POSE_PAIRS_BODY_25 = [[0, 1], [0, 15], [0, 16], [1, 2], [1, 5], [1, 8], [8, 9], [8, 12], [9, 10], [12, 13], [2, 3],
+                      [3, 4], [5, 6], [6, 7], [10, 11], [13, 14], [15, 17], [16, 18], [14, 21], [19, 21], [20, 21],
+                      [11, 24], [22, 24], [23, 24]]
+
+
+# 클래스 리스트
+classes = ["person", "bicycle", "car", "motorcycle",
+           "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+           "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+           "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+           "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis",
+           "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+           "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife",
+           "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog",
+           "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table",
+           "toilet", "tv", "laptop", "mouse", "remote", "keyboard",
+           "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+           "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
